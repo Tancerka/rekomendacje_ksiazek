74b88c354904e579f3783c156ae45c090c1696f6
Tancerka <victoria.morawiec2412@gmail.com>
2025-10-13 18:26:49 +0200

    /html+css/: 13.10/: add navigation bar (sketch), add nav.html, recommend.html, wishlist.html

--- run.py
+++ run.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/recommend')
+def recommend():
+    return render_template('recommend.html')
+
+@app.route('/wishlist')
+def wishlist():
+    return render_template('wishlist.html')
