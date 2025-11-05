cf4e9fecf33730805f73f87ed767fe95885f6792
Victoria Morawiec <victoria.morawiec2412@gmail.com>
2025-11-05 13:10:31 +0100

    /db/: 05.11/: registration input validation

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -28,9 +28,27 @@ def register():
         username = request.form.get('username')
         email = request.form.get('email')
         password = request.form.get('password')
-        from app import mongo
-        user_id = mongo.db.users.insert_one({'username':username, 'email':email, 'password':password})
-        return redirect(url_for('auth.login'))
+        domain = email.split('@')
+        #check if the email has at least 1 @, the part before the last @ is non-empty, domain contains at least one .
+        dot = email.count('@')
+        point = email.count('.')
+        user_data = find_user_by_username(username)
+        print("username after split: ", domain)
+        print("dot count: ", dot)
+        print("point count: ", point)
+        print("local-part", domain[0])
+        print("user data", user_data.get('email'))
+        if dot != 0 and point != 0 and domain[1] != None:
+            if user_data.get('email') == email:
+                message = "Taki użytkownik już istnieje."
+                return render_template('register.html', message = message)
+            else:
+                from app import mongo
+                user_id = mongo.db.users.insert_one({'username':username, 'email':email, 'password':password})
+                return redirect(url_for('auth.login'))
+        else:
+            message = "Podany email jest niepoprawny."
+            return render_template('register.html', message = message)
     return render_template('register.html')
 
 @auth_bp.route('/login', methods=['GET', 'POST'])
@@ -40,6 +58,8 @@ def login():
         password = request.form.get('password')
         if check_password(username, password):
             user_data = find_user_by_username(username)
+            #if user_data
+                
             user = User(str(user_data['_id']), user_data['username'], user_data['email'])
             login_user(user)
             return redirect(url_for('index'))
